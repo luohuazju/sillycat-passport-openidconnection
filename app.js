b4db47316cb9cbf9ae563993ad9d801e4683d98b
Hua Luo <hluo@lifesize.com>
2020-04-24 15:44:46 -0500

    add dependecy

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var passport = require('passport');
+var OidcStrategy = require('passport-openidconnect').Strategy;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
