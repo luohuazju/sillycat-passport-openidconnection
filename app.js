12a10a135ef99ac9ca03d24bbf01011e4c359aec
Hua Luo <hluo@lifesize.com>
2020-04-24 18:11:18 -0500

    working for okta

--- app.js
+++ app.js
@@ -16,14 +16,67 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+function ensureLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  res.redirect('/login')
+}
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: 'mysessionsecret',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use('oidc', new OidcStrategy({
+  issuer: process.env.OKTA_ISSUER,
+  authorizationURL: process.env.OKTA_AUTHORIZATION_URL,
+  tokenURL: process.env.OKTA_TOKEN_URL,
+  userInfoURL: process.env.OKTA_USERINFO_URL,
+  clientID: process.env.OKTA_CLIENT_ID,
+  clientSecret: process.env.OKTA_CLIENT_SECRET,
+  callbackURL: 'http://localhost:3000/authorization-code/callback',
+  scope: 'openid profile'
+}, (issuer, sub, profile, accessToken, refreshToken, done) => {
+  return done(null, profile);
+}));
+
+passport.serializeUser((user, next) => {
+  next(null, user);
+});
+
+passport.deserializeUser((obj, next) => {
+  next(null, obj);
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login', passport.authenticate('oidc'));
+app.use('/authorization-code/callback',
+  passport.authenticate('oidc', { failureRedirect: '/error' }),
+  (req, res) => {
+    res.redirect('/profile');
+  }
+);
+app.use('/profile', ensureLoggedIn, (req, res) => {
+  res.render('profile', { title: 'Express', user: req.user });
+});
+app.get('/logout', (req, res) => {
+  req.logout();
+  req.session.destroy();
+  res.redirect('/');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
